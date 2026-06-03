733e88f51f6892b9c272fe3a8b4cccf94eed1933
choOijian <choijian@sookmyung.ac.kr>
2026-06-04 03:12:31 +0900

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const html = fs.readFileSync('index.html', 'utf8');
-const dom = new JSDOM(html, { runScripts: "dangerously", resources: "usable" });
-setTimeout(() => {
-  console.log(dom.window.testData ? "YES" : "NO");
-}, 1000);
